2e04745084249fd1cbd6544c6b1e7198e9037b8b
Subhash Rana <Subhash100393@gmail.com>
2026-06-04 11:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (75.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // optimize approach to solve anagram
         int n = s.size();
         int m = t.size();
+
         if(n != m) return false;
 
-        unordered_map<char, int>mp1;
-        unordered_map<char, int>mp2;
+        vector<int>count(26, 0);
 
-        for(int i = 0; i < n; i++)
-            mp1[s[i]]+=1;
-        
-        for(int j = 0; j < m; j++)
-            mp2[t[j]]+=1;
+        for(int i = 0; i < n; i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
 
-        for(auto &it: mp1){
-            if(mp2[it.first] != it.second)
-                return false;         
+        for(int it : count){
+            if( it != 0) return false;
         }
 
         return true;
-        
     }
 };
