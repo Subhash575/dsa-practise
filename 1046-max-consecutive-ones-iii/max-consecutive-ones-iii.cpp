ec6531df244069391c582288636f145d71b089d7
Subhash Rana <Subhash100393@gmail.com>
2026-06-07 13:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (58.99%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-
+    int longestOnes(vector<int>& arr, int k) {
+        // optimize approach:- Here we remove the inner-while loop by mentain
+        //  the constant window after getting the extra zero
+        int n = arr.size();
         int maxLen = 0;
-        int zeroCnt = 0;
         int i = 0, j = 0;
-        while(j < n){
-            if(nums[j] == 0) zeroCnt+=1;
-            if(zeroCnt <= k){
-                maxLen = max(maxLen, j - i + 1);
-            }else {
-                if(nums[i] == 0){
-                    zeroCnt -=1;
-                }
-                i++;     
+        int zeroCnt = 0;
+
+        while (j < n) {
+            if (arr[j] == 0)
+                zeroCnt += 1;
+            
+            if(zeroCnt > k){
+                if(arr[i] == 0)
+                   zeroCnt-=1;
+                i+=1;
             }
+
+            if(zeroCnt <= k){
+               maxLen = max(maxLen, j - i + 1);
+            }
+
             j++;
+
         }
-        
-        return maxLen;
+
+        return maxLen;    
     }
 };
