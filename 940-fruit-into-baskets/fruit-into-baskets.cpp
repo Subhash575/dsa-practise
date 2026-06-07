9ad6376d65ed45e4e578f2e10607858e3b3f71cc
Subhash Rana <Subhash100393@gmail.com>
2026-06-07 16:48:17 +0530

    Time: 49 ms (33.75%) | Memory: 84.9 MB (34.10%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -9,18 +9,14 @@ public:
         while (j < n) {
             mpp[arr[j]] += 1;
             if (mpp.size() > 2) {
-                while (mpp.size() > 2) {
-                    mpp[arr[i]] -= 1;
-                    if (mpp[arr[i]] == 0) {
-                        mpp.erase(arr[i]);
-                        i += 1;
-                        break;
-                    }
-                    i += 1;
-                }
+                mpp[arr[i]] -= 1;
+                if (mpp[arr[i]] == 0)
+                    mpp.erase(arr[i]);
+                i += 1;
             }
 
-            maxLen = max(maxLen, j - i + 1);
+            if (mpp.size() <= 2)
+                maxLen = max(maxLen, j - i + 1);
             j++;
         }
 
