a7f3cac97d81601ad53e74f689401289fe68cf91
Subhash Rana <Subhash100393@gmail.com>
2026-06-08 23:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (8.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-
         int n = nums.size();
-        vector<int>ans(n, 0);
-        int multi = 1;
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0)
-               count+= 1;
 
-            if(count <= 1 && nums[i] != 0){
-                multi = multi*nums[i];
-            }
-           
-           
+        // This is the Better approach for  solving this problem without 
+        // using the division
+        // Here we build the prefix and suffix which have the multiple of
+        // previous and current element.
+
+        vector<int>prefix(n, 1);
+        vector<int>suffix(n, 1);
+
+        // Building prefix:-
+        for(int i = 1; i < n; ++i)
+            prefix[i] = prefix[i-1]*nums[i-1];
+        
+        for(int i = n-2; i >= 0; --i){
+            suffix[i] = suffix[i+1]*nums[i+1];
         }
 
-        if(count > 1) return ans;
-
-        for(int i = 0; i < n; i++){
-            if(count == 1 && nums[i] == 0)
-                ans[i] = multi;
-            else if(count == 1 && nums[i] != 0)
-                continue;
-            else{
-                ans[i] = multi/nums[i];
-            }
+        vector<int>ans(n, 0);
+        for(int i = 0; i < n; ++i){
+            ans[i] = prefix[i]*suffix[i];
         }
 
         return ans;
