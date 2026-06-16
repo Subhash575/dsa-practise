b612523bb868d3fa754d6fd42064027e249f35cd
Subhash Rana <Subhash100393@gmail.com>
2026-06-16 11:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (45.77%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    // Recursive approach
+    int bs(vector<int>&arr, int low, int high, int target){
+
+        if(low > high) return -1;
+
+        int mid = low + (high -low)/2;
+        if(arr[mid] == target) return mid;
+
+        if(arr[mid] > target) return bs(arr, low, mid-1, target);
+        else return bs(arr, mid+1, high, target);
+
+    }
+
     int search(vector<int>& arr, int target) {
-        // iterative approach
-        int n = arr.size();
-        int low = 0, high = n-1;
-        int mid;
-        while(low <= high){
-            mid = low + (high-low)/2;
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] > target) high = mid-1;
-            else low = mid + 1;
-        }
-        return -1;  
+        return bs(arr, 0, arr.size()-1, target);
     }
 };
