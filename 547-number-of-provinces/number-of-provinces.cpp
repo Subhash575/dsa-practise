d12cbcee63ba7b6d41e8a94741669e79dd9940e0
Subhash Rana <Subhash100393@gmail.com>
2026-07-01 11:27:18 +0530

    Time: 3 ms (37.32%) | Memory: 19.6 MB (55.41%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
 
-    void bfs(vector<vector<int>>&adj, vector<int>&vis, queue<int>&q, int node){
-        q.push(node);
+    void traversal(int node, vector<vector<int>>&graph, vector<int>&vis){
         vis[node] = 1;
-        while(!q.empty()){
-            int f = q.front();
-            q.pop();
-            for(int nbr: adj[f]){
-                if(!vis[nbr]){
-                    q.push(nbr);
-                    vis[nbr] = 1;
-                }
+
+        for(int nbr = 0; nbr < graph.size(); nbr++){
+            if(graph[node][nbr] == 1 && vis[nbr] == 0){
+                traversal(nbr, graph, vis);
             }
-
         }
-
-        return;
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
+
+        // Using DFS and adjMatrix.
         int n = isConnected.size();
         vector<int>vis(n, 0);
         int res = 0;
 
-        vector<vector<int>>adj(n);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i != j && isConnected[i][j] ){
-                    adj[i].push_back(j);
-                }
-            }
-        }
-
-        queue<int>q;
-
         for(int i = 0; i < n; i++){
             if(!vis[i]){
                 res+=1;
-               bfs(adj, vis, q, i);
+                int node = i;
+                traversal(node, isConnected, vis);
             }
         }
-
+        
         return res;
-      
+        
     }
 };
