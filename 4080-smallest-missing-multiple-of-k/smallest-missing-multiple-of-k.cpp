389b2c7cac557c062ce95b057fb99808462d7069
Subhash Rana <Subhash100393@gmail.com>
2026-08-25 11:18:37 +0530

    Time: 7 ms (9.55%) | Memory: 25 MB (53.82%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
+        // Optimize solution:- using unordered set, and checking
+        // multiple count exist in there or not.
+        unordered_set<int>st (nums.begin(), nums.end());
+
         int multi = k;
-        int n = nums.size();
-
-        int res = -1;
-        int cnt = 1;
-
-        while(true){
-            
-            int i = 0;
-            bool isExist = false;
-            while(i < n){
-                if(nums[i] == multi){
-                    isExist = true;
-                    cnt+=1;
-                    break;
-                }
-                   
-               i++;    
-            }
-
-            if(isExist == false){
-                res = multi;
-                break;
-            }else{
-                multi = k*cnt;
-            }
-
+        while(st.count(multi)){
+            // updating to next multiple
+            multi += k;
         }
-
-        return res;
+        return multi;
+        
     }
 };
