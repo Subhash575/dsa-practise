82b38953e3beea35d43e9decd39c2706d3f48e4a
Subhash Rana <Subhash100393@gmail.com>
2026-09-10 00:47:11 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (25.59%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
 
+        // 1 based indexing
         int n = isConnected.size();
-        vector<vector<int>>graph(n);
+        vector<vector<int>>graph(n+1);
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(i != j && isConnected[i][j]){
-                  graph[i].push_back(j);
-                  graph[j].push_back(i);
+                  graph[i+1].push_back(j+1);
                 }
             }
         }
 
         int res = 0;
-        vector<int>vis(n);
+        vector<int>vis(n+1, 0);
         queue<int>q;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             if(!vis[i]){
                 res += 1;
                 dfsTraversal(graph, vis, i);
