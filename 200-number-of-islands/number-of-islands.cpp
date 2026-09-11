306e0e720a898c98f0c2c71f428d2b0dda4bf882
Subhash Rana <Subhash100393@gmail.com>
2026-09-12 00:38:40 +0530

    Time: 25 ms (71.36%) | Memory: 19.2 MB (35.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-   
-   void traversal(int row, int col, vector<vector<char>>&grid, 
-    vector<vector<int>>&vis ){
-        int m = grid.size();
-        int n = grid[0].size();
-        vis[row][col] = 1;
-        // traversal accross the neighbour
-        for(int delRow = -1; delRow <= 1; delRow++){
-            for(int delCol = -1; delCol <= 1; delCol++){
-                if(abs(delRow) == abs(delCol)) continue;
-
-                int nbrRow = row + delRow;
-                int nbrCol = col + delCol;
-
-                if(nbrRow >= 0 && nbrRow < m && nbrCol >= 0 && nbrCol < n
-                && vis[nbrRow][nbrCol] == 0 && grid[nbrRow][nbrCol] == '1'){
-                    traversal(nbrRow, nbrCol, grid, vis);
-                }
-            }
-        }
-    
-    }
-
-
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>>vis(m, vector<int>(n, 0));
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n, vector<int>(m, 0));
         int res = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    res+=1;
-                    traversal(i, j, grid,vis);
+
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    res += 1;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                    while (!q.empty()) {
+                        int row = q.front().first;
+                        int col = q.front().second;
+                        q.pop();
+
+                        for (int delrow = -1; delrow <= 1; delrow++) {
+                            for (int delcol = -1; delcol <= 1; delcol++) {
+                                if (abs(delrow) == abs(delcol))
+                                    continue;
+
+                                int nbrRow = row + delrow;
+                                int nbrCol = col + delcol;
+
+                                if (nbrRow >= 0 && nbrRow < n && nbrCol >= 0 &&
+                                    nbrCol < m && !vis[nbrRow][nbrCol] &&
+                                    grid[nbrRow][nbrCol] == '1') {
+                                    q.push({nbrRow, nbrCol});
+                                    vis[nbrRow][nbrCol] = 1;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        return res;   
+
+        return res;
     }
 };
